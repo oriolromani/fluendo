7371b985a7893573ea159459c72f0522d7c46e09
Oriol Romani <oromani@bmat.com>
2021-07-23 16:37:29 +0200

    Add user mangement endpoints  for html app

--- fluendo/settings.py
+++ fluendo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'flutodo_api.apps.FlutodoConfig',
     'rest_framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'fluendo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'flutodo/templates'],
+        'DIRS': [BASE_DIR / 'flutodo/templates', BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
