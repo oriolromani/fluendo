723ec8373468224d0ee4b48e3a9290f1efcbe683
Oriol Romani <oromani@bmat.com>
2021-07-25 23:36:20 +0200

    Document the API with Swagger/OpenAPI

--- flutodo_api/urls.py
+++ flutodo_api/urls.py
@@ -1,9 +1,28 @@
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from . import views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Flutodo REST API",
+      default_version='v1',
+      description="Dummy description",
+      contact=openapi.Contact(email="oriolromanipicas"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', views.ToDoItemList.as_view()),
     path('create', views.TodoItemCreate.as_view()),
     path('<int:pk>', views.ToDoItemDetail.as_view()),
     path('accounts/', include('rest_registration.api.urls')),
+    path('playground/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
--- flutodo_api/views.py
+++ flutodo_api/views.py
@@ -5,18 +5,27 @@ from rest_framework import generics
 
 
 class ToDoItemList(generics.ListAPIView):
+    """
+    Returns a list of all ToDoItems
+    """
     permission_classes = [permissions.IsAuthenticated]
     queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemSerializer
 
 
 class TodoItemCreate(generics.CreateAPIView):
+    """
+    Create a ToDoItem
+    """
     permission_classes = [permissions.IsAuthenticated]
     queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemCreateSerializer
 
 
 class ToDoItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or remove a ToDoItem
+    """
     permission_classes = [permissions.IsAuthenticated]
     queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemSerializer
